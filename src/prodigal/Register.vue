51361543b91d12c7805657b79b96e3df8fbbe7c3
motcs <motcs@qq.com>
2021-10-06 12:20:19 +0800

    注册界面实现界面判断逻辑

--- src/prodigal/Register.vue
+++ src/prodigal/Register.vue
@@ -1,8 +1,15 @@
 <template>
-  <!-- <img src="./assets/logo.png" /> -->
   <el-container>
     <el-main> </el-main>
-    <el-aside class="regis">
+
+    <el-aside>
+      <p>欢迎注册-浪子归家</p>
+      <p>我们致力于交流学习</p>
+      <p id="register-p-left">
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;现在学习会遇到很多不会的，往往一个很简单的问题，没有思路却要花费很久的时间才能达到我们想要的效果.
+        所以，来和我们试一试把
+      </p>
+      <el-divider></el-divider>
       <div id="register-submit-from">
         <el-form ref="ruleForm" :model="ruleForm" status-icon :rules="rules">
           <el-form-item prop="username">
@@ -30,8 +37,11 @@
           </el-form-item>
 
           <el-form-item>
-            <el-button type="primary" @click="submitForm('ruleForm')"
-              >Submit</el-button
+            <el-button
+              id="register-submit-size"
+              type="primary"
+              @click="submitForm('ruleForm')"
+              >立即注册</el-button
             >
           </el-form-item>
         </el-form>
@@ -43,7 +53,10 @@
 </template>
 
 <script>
-export default {
+import { defineComponent } from "vue";
+import { ElMessageBox, ElMessage } from "element-plus";
+import { useRouter } from "vue-router";
+export default defineComponent({
   data() {
     const checkUsername = (rule, value, callback) => {
       if (value === "") {
@@ -87,27 +100,58 @@ export default {
       },
     };
   },
+  setup() {
+    const router = useRouter();
+
+    const open = () => {
+      router.push("/");
+    };
+
+    return {
+      open,
+    };
+  },
   methods: {
     submitForm(formName) {
       this.$refs[formName].validate((valid) => {
         if (valid) {
-          alert("submit!");
+          ElMessageBox.confirm("注册成功了，是否现在转去登录呢?", "浪子归家", {
+            confirmButtonText: "好的",
+            cancelButtonText: "取消",
+            type: "success",
+          }).then(() => {
+            this.open();
+          });
         } else {
-          console.log("error submit!!");
+          ElMessage.error('注册失败了，可能是网络出现了异常，请稍后重试.')
           return false;
         }
       });
     },
   },
-};
+});
 </script>
 
 <style>
 #register-submit-from {
   position: relative;
-  width: 350px;
+  width: 23em;
   height: auto;
-  border: red 1px solid;
+  /* border: red 1px solid; */
+}
+p {
+  color: black;
+}
+html {
+  overflow: hidden;
+}
+
+#register-p-left {
+  text-align: left;
+}
+
+#register-submit-size {
+  width: 26em;
 }
 
 /* 容器样式初始化 */
@@ -120,19 +164,22 @@ export default {
 }
 
 .el-aside {
-  background-color: #d3dce6;
+  background: white no-repeat center 0;
   color: var(--el-text-color-primary);
   text-align: center;
-  width: 352px;
-  height: 100%;
+  width: 23em;
+  margin-left: 4px;
+  height: calc(100vh);
 }
 
 .el-main {
-  background-color: #e9eef3;
-  opacity: 0.4;
+  /* background-color: #e9eef3; */
+  background: url("../assets/register/hailang.jpg") no-repeat center 0;
+  background-size: 100%;
+  opacity: 0.9;
   color: var(--el-text-color-primary);
   text-align: center;
-  line-height: 160px;
+  margin: 0 auto;
 }
 
 body > .el-container {
